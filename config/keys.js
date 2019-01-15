ec519f6077f01fed44780d4520ae9921099bf83e
RNMancini <41347680+RNMancini@users.noreply.github.com>
2019-01-14 19:42:20 -0500

    new update of environment

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Rebecca:Rebecca1@ds119588.mlab.com:19588/socialnetwork1"
+  mongoURI: "mongodb://Rebecca:Rebecca1@ds119588.mlab.com:19588/socialnetwork1",
+  secretOrKey: "secret"
 };
