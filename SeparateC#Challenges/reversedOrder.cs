ec4679485a6cf99c362ff3940adb3aa2f8ce9e01
RNMancini <41347680+RNMancini@users.noreply.github.com>
2019-03-16 12:27:55 -0400

    C# Challenge-Reverse String first test

---
+++ SeparateC#Challenges/reversedOrder.cs
@@ -0,0 +1,22 @@
+// create a fuction that will take an integer and reverse the order
+
+// ex
+// Input: 21445 Output: 54412
+
+// Input: 145263 Output: 362541
+
+// Input: 1254859723 Output: 3279584521
+using System;
+public static class Kata
+{
+  public static int ReversedOrder(int num)
+  {
+    int Reverse = 0;
+    while(Number>0)
+    {
+      Reverse = Reverse*10 + Number%10;
+      Number /=10;
+    }
+    return 0;
+  }
+}
