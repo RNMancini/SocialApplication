f5f3311737d5c2728949d5f25e12f396d8ce2e6a
RNMancini <41347680+RNMancini@users.noreply.github.com>
2019-04-02 23:51:18 -0400

    board additions

---
+++ SeparateC#Challenges/fareCalculation.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Text;
+
+double[] fareEstimator(int ride_time, int ride_distance, double[] cost_per_minute, double[] cost_per_mile) {
+  double[] results = new double[cost_per_mile.Length];
+
+  Console.WriteLine(results[]);
+  return results;
+} 
