b608498542d761472de85820b070ce2af1811692
RNMancini <41347680+RNMancini@users.noreply.github.com>
2019-06-10 23:59:42 -0400

    Delete route modification.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,24 +3,20 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-//@route  GET api/posts/test
-//@desc   Tests post route
-//@access Public
-
-
 // Post model
 const Post = require('../../model/Post');
 
 // Validation
 const validatePostInput = require('../../validation/post');
 
-// Profile model
-const Profile = require('../../models/Profile');
+//@route         GET api/posts/test
+//@description   Tests post route
+//@access        Public
+router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
-// @route  GET api/posts/test
-// @desc   TESTS post route
-// @access PUBLIC
-router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+//@route         GET api/posts
+//@description   Get posts 
+//@access        Public
 
 router.get('/', (req, res) => {
   Post.find()
@@ -29,19 +25,41 @@ router.get('/', (req, res) => {
   .catch(err => res.status(404).json({ nopostsfound: 'No post found' }));
 });
 
-router.get('/', (req, res) => {
-  Post.find()
-  .sort({ date: -1 })
-  .then(posts => res.json(posts))
-  .catch(err => res.status(404).json({ nopostfound: 'No post found with that ID' }));
+// @route       GET api/posts/:id
+// @description Get post by id
+// @access      Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+  .then(post => res.json(post))
+  .catch(err => res.status(404).json({ nopostfound: 'No post found with that ID' })
+  );
 });
 
+// @route      POST api/posts
+// @descripton Create post
+// @access     Private
 router.post(
   '/', 
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
      const { errors, isValid } = validatePostInput(req.body);
-  );
+  
+// Check Validation
+if (!isValid) {
+  // If any errors, send 400 with errors object
+  return res.status(400).json(errors);
+}
+
+const newPost = new Post({
+  text: req.body.text,
+  name: req.body.name,
+  avatar: req.body.avatar,
+  user: req.user.id
+});
+
+newPost.save().then(post => res.json(post));
+  }
+);
 
 // @route  Delete api/posts/:id
 // @desc   Delete post  
@@ -110,7 +128,7 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }),
          if(post.likes.filter(like => like.user.toString() === req.user.id).length == 0) {
            return res.status(400).json({ notliked: 'You have not yet liked this post' });
          } 
-         //post
+         
          // Get remove index
          const removeIndex = post.likes
          .map(item => item.user.toString())
@@ -130,9 +148,9 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }),
   }
 );
 
-//@Route POST api/posts/comment/:id
-// @comment to post
-// @access Private
+// @route       POST api/posts/comment/:id
+// @description Add comment to post
+// @access      Private
 router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
   Post.findById(req.params.id)
   .then(post => {
@@ -166,6 +184,20 @@ router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (r
             }
 
             // Add to comments array
+// @route  POST api/posts/comment/:id
+// @desc   Add comment to post
+// @access Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Post.findById(req.params.id)
+      .then(post => {
+          const NewComment = {
+              text: req.body.text,
+              name: req.body.name,
+              avatar: req.body.avatar,
+              user: req.user.id
+          }
+
+          // Add to comments array
 
             
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,10 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text) {
+
+  }
+}
